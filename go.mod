3d440b26d38a2f17e9565737144b1b2f9982ccb2
Qianjin Xu <xuqianjinchn@gmail.com>
2023-12-21 16:14:13 +0800

    Create hello Go Modules

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/qianjinxu/hello
+
+go 1.21
+
+replace github.com/qianjinxu/greetings => ../greetings
+
+require github.com/qianjinxu/greetings v0.0.0-00010101000000-000000000000
---
+++ hello.go
@@ -0,0 +1,17 @@
+/*
+ * GitHub: github.com/qianjinxu
+ * Email: xuqianjinchn@gmail.com
+ * Bio: https://jin.bio
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/qianjinxu/greetings"
+)
+
+func main() {
+	message := greetings.Hello("Qianjin Xu")
+	fmt.Println(message)
+}
