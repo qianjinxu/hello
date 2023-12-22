1e214dd52dadda8882aa8494363ec800bead84f8
Qianjin Xu <xuqianjinchn@gmail.com>
2023-12-22 16:49:06 +0800

    Use Slice

--- hello.go
+++ hello.go
@@ -16,9 +16,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Qianjin Xu")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-  fmt.Println(message)
+
+	fmt.Println(message)
 }
